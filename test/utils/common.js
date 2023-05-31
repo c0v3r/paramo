3970da3b1624f022ff8cb24fbc3c26084aaf8320
c0v3r <cover.hm@gmail.com>
2023-05-31 18:08:19 -0300

    code updated

--- test/utils/common.js
+++ test/utils/common.js
@@ -20,48 +20,5 @@ class Common {
             }
         }
     }
-
-    /**
-     * Function to execute common steps in the login feature
-     */
-    async loginFlow() {
-        const SplashPage = require("../pages/splashPage.js")
-        const LoginPage = require("../pages/loginPage.js")
-        const { expect } = require('chai');
-        const Login = require("../fixtures/users.json")
-
-        let splashPage = new SplashPage();
-        let loginPage = new LoginPage()
-
-        await browser.pause(5000);
-        expect(true)
-            .to.equal(await splashPage.getDeepBlueImg.isDisplayed(), "Splash page was not displayed.")
-        await browser.pause(1000);
-        await splashPage.getSignInLink.click()
-
-        expect(await loginPage.getSignInToDeepBlueLbl.getText()).
-            to.equal('Sign in to DeepBlue', "Login page was not displayed.")
-        await loginPage.login(Login['validUser'])
-    }
-
-
-    /**
-     * Function to handle the error screen (Android only)
-     */
-    async errorScreenHandler() {
-        const SplashPage = require("../pages/splashPage.js")
-        const LoginPage = require("../pages/loginPage.js")
-        const { expect } = require('chai');
-        const Login = require("../fixtures/users.json")
-
-        let loginPage = new LoginPage()
-
-        if (loginPage.getSignInButton.isDisplayed() == true) {
-            console.log('SignInButton is still displayed')
-            await browser.pause(2000);
-        }
-        let timeout = 30
-        await this.waitForDisapeears("//*[@text='Error loading page']", timeout)
-    }
 }
 export default new Common();
